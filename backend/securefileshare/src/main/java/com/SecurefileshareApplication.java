a8ce79f0ff4d3d90c12ab2defd440736943654c4
Payal <payalrajgor.010900@gmail.com>
2026-04-28 16:28:47 +0530

    Initial full project setup (backend + frontend)

---
+++ backend/securefileshare/src/main/java/com/SecurefileshareApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurefileshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurefileshareApplication.class, args);
+		System.out.println("Application Started!!");
+	}
+
+}
